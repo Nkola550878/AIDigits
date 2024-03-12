cf341dbd2e07ea0ccee09c8c243f47ffab6d91c1
Nikola Kosanovic <nklksnvc@gmail.com>
2024-03-12 23:31:31 +0100

    Basic drawing

--- Assets/DrawingCanvas.cs
+++ Assets/DrawingCanvas.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Properties;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.UI;
 
 public class DrawingCanvas : MonoBehaviour
 {
+    Vector3 center;
+    Vector3 mousePos;
+
     [SerializeField] int dimension;
     [SerializeField] Sprite pixel;
     [SerializeField] float scale;
     [SerializeField] Transform pivot;
-    Vector3 center;
+    [SerializeField] Camera camera;
+    [SerializeField] Slider radiusSlider;
+    [SerializeField] Slider strengthSlider;
+
+    [Header("Crtanje")]
+
+    [SerializeField] float radius = 2;
+    [SerializeField] float strength = 1f;
 
     void Start()
     {
@@ -48,8 +60,65 @@ public class DrawingCanvas : MonoBehaviour
         }
     }
 
+    float Strength(float d)
+    {
+        return Mathf.Max(strength * (1 - 1 * d / radius), 0);
+    }
+
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            if(mousePos != Input.mousePosition)
+            {
+                Paint();
+                mousePos = Input.mousePosition;
+                Debug.Log("a");
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mousePos = new Vector3(0, 0, 1);
+        }
         center = pivot.position;
+    }
+
+    void Paint()
+    {
+        Vector2 mousePosition = new Vector2(camera.ScreenToWorldPoint(Input.mousePosition).x, camera.ScreenToWorldPoint(Input.mousePosition).y);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            for (int j = 0; j < transform.GetChild(i).childCount; j++)
+            {
+                GameObject current = transform.GetChild(i).GetChild(j).gameObject;
+                float distance = Vector2.Distance(current.transform.position, mousePosition);
+                float gray = Strength(distance);
+                Color color = new Color(gray, gray, gray, 0);
+                current.GetComponent<SpriteRenderer>().color -= new Color(gray, gray, gray, 0);
+                //Debug.Log($"{distance}, {gray}");
+                //Debug.Log($"{current.GetComponent<SpriteRenderer>().color} - {color}");
+            }
+        }
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            for (int j = 0; j < transform.GetChild(i).childCount; j++)
+            {
+                transform.GetChild(i).GetChild(j).gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+    }
+
+    public void ReadRadius()
+    {
+        radius = radiusSlider.value;
+    }
+
+    public void ReadStrength()
+    {
+        strength = strengthSlider.value;
     }
 }
